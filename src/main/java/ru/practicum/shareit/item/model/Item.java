a45a62df29e0fe23760348da0b1c9ebd02e19994
Egor Fortov <egorfortov@gmail.com>
2025-01-05 20:30:53 +0300

    Item class done

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,22 @@
 package ru.practicum.shareit.item.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import ru.practicum.shareit.user.User;
+
 /**
  * TODO Sprint add-controllers.
  */
 public class Item {
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String description;
+
+    private boolean isAvailable;
+    private User owner;
+    private String request;
 }
